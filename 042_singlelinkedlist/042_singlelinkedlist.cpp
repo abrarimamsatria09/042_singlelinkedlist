bd597482e657a89dcf6a4d5bce99787edb8ee256
abrarimamsatria09 <092004abrar@gmail.com>
2023-05-18 18:53:11 +0700

    untuk memproses pilihan pengguna ketika mereka memilih opsi 2

--- 042_singlelinkedlist/042_singlelinkedlist.cpp
+++ 042_singlelinkedlist/042_singlelinkedlist.cpp
@@ -151,5 +151,13 @@ int main() {
 				system("pause");
 				system("cls");
 				break;
+			case 2:
+				if (listEmpty())
+				{
+					cout << "List kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
 
 			
